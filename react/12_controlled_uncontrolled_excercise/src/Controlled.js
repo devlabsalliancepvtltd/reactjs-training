fc35e6f137471c5010c71978f759131d416a94f6
unknown <sreekanthme@gmail.com>
2022-11-17 11:05:25 +0530

    forms excercise

---
+++ react/12_controlled_uncontrolled_excercise/src/Controlled.js
@@ -0,0 +1,127 @@
+import React from "react";
+
+const Controlled = () => {
+  const [formData, setFormData] = React.useState({
+    text: "default text",
+    select: "2",
+    radio: "F",
+    check: {
+      hyderbad: false,
+      bangalore: true,
+    },
+    textarea: "some default text",
+  });
+
+  const textChanged = (e) => {
+    const value = e.target.value;
+
+    setFormData((prevState) => {
+      return { ...prevState, text: value };
+    });
+  };
+
+  const selectChanged = (e) => {
+    const value = e.target.value;
+
+    setFormData((prevState) => {
+      return { ...prevState, select: value };
+    });
+  };
+
+  const textAreaChanged = (e) => {
+    const value = e.target.value;
+
+    setFormData((prevState) => {
+      return { ...prevState, textarea: value };
+    });
+  };
+
+  const checkChanged = (e, city) => {
+    const value = e.target.value;
+
+    // TODO:
+  };
+
+  const radioChanged = (value) => {
+    setFormData((prevState) => {
+      return { ...prevState, radio: value };
+    });
+  };
+
+  const submitForm = () => {
+    console.log("Form data: ", formData);
+  };
+
+  return (
+    <div>
+      <div>
+        <input type="text" value={formData.text} onChange={textChanged} />
+        <br /> <br />
+      </div>
+
+      <div>
+        <select value={formData.select} onChange={selectChanged}>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </select>
+        <br /> <br />
+      </div>
+
+      <div>
+        <input
+          type="radio"
+          checked={formData.radio === "M" ? true : false}
+          onChange={() => {
+            radioChanged("M");
+          }}
+        />
+        Male &nbsp;&nbsp;&nbsp;&nbsp;
+        <input
+          type="radio"
+          checked={formData.radio === "F" ? true : false}
+          onChange={() => {
+            radioChanged("F");
+          }}
+        />
+        Female
+        <br /> <br />
+      </div>
+
+      {/*<div>
+         <input
+          type="checkbox"
+          checked={formData.check.includes("Hyderabad") ? true : false}
+          onChange={(e) => {
+            checkChanged(e, "Hyderabad");
+          }}
+        />
+        Hyderabad&nbsp;&nbsp;&nbsp;&nbsp;
+        <input
+          type="checkbox"
+          checked={formData.check.includes("Bangalore") ? true : false}
+          onChange={(e) => {
+            checkChanged(e, "Bangalore");
+          }}
+        />
+        Bangalore
+        <br /> <br />
+      </div> */}
+
+      <div>
+        <textarea
+          value={formData.textarea}
+          onChange={textAreaChanged}
+        ></textarea>
+        <br /> <br />
+      </div>
+
+      <div>
+        <button onClick={submitForm}>Submit Form</button>
+        <br /> <br />
+      </div>
+    </div>
+  );
+};
+
+export default Controlled;
